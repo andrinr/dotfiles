edc563fc00b06dd54572e6cae77266bd8e19674c
andrinr <andrinrehmann@gmail.com>
2024-03-28 15:10:17 +0100

    setup

--- setup.bash
+++ setup.bash
@@ -2,10 +2,11 @@
 git config --global user.email "andrinrehmann@gmail.com"
 git config --global user.name "andrinr"
 
+# file structure
 if ! [ -d ~/projects]; then
   mkdir ~/projects
 fi
 
+# ssh key
 ssh-keygen
-
 cat ~/.ssh/id_rsa.pub
